814700f704aad902fbcb1297d0086917e879e0fc
Ajay Digital Dreamworks <autenicyt@gmail.com>
2025-12-28 19:34:44 +0530

    new

--- utils/nodemailer.js
+++ utils/nodemailer.js
@@ -1,34 +1,23 @@
-const nodemailer = require("nodemailer");
+const nodemailer = require('nodemailer');
+require("dotenv").config();
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS, // Gmail App Password
+    pass: process.env.EMAIL_PASS,
   },
-  tls: {
-    rejectUnauthorized: false,
-  },
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 10000,
-});
-
-// Test SMTP (Render logs me dikhega)
-transporter.verify((err) => {
-  if (err) console.error("SMTP ERROR:", err);
-  else console.log("SMTP READY");
 });
 
 async function sendOTP(email, otp) {
-  await transporter.sendMail({
+  const mailOptions = {
     from: process.env.EMAIL_USER,
     to: email,
     subject: "ClassTalk OTP Code",
     text: `Your OTP is ${otp}. It expires in 5 minutes. Please do not share this code with anyone.`,
-  });
+  };
+
+  await transporter.sendMail(mailOptions);
 }
 
 module.exports = sendOTP;
